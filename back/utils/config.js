74842e918a4bb973805ef42e864031efbb4185e8
Martin Fiori <martin.fiori@jphlions.com>
2023-09-12 16:55:06 -0300

    adding db connection

---
+++ back/utils/config.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env
+
+module.exports = {
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE
+}
