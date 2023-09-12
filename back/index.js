5e5ce546fe6d602c0101ea5fbd0e0d7ca5a8f680
Martin Fiori <martin.fiori@jphlions.com>
2023-09-12 16:42:49 -0300

    setting up express server

---
+++ back/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(8080, () => console.log(`Listening on port ${PORT}`))
