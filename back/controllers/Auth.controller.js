34173e374528c42eec3c939c0fc5092a672be710
MartinFiori <martinfiori@outlook.com>
2023-09-13 00:03:36 -0300

    emailvalidator added in signup and login method done

--- back/controllers/Auth.controller.js
+++ back/controllers/Auth.controller.js
@@ -6,19 +6,21 @@ const {
 } = require("../utils/config.js");
 const db = require("../db.js");
 const { createHash, isSamePwd } = require("../utils/hash/hash.js");
+const isValidEmail = require("../utils/isValidEmail.js");
 
 class Auth {
   static signup(req, res) {
     const { username, password, email } = req.body;
     if (![username, password, email].every(Boolean))
       return res.json({ error: "Please complete all inputs." });
+    if (!isValidEmail(email))
+      return res.json({ error: "Email format invalid." });
     try {
       const userFoundQuery = `SELECT * FROM ${users_table} WHERE email = ?`;
       db.query(userFoundQuery, [email], (err, result) => {
         if (err) return res.json({ error: err.message });
         if (result.length) return res.json({ error: "User already exists." });
         const createUserQuery = `INSERT INTO ${users_table}( email, password, username ) VALUES(?)`;
-
         db.query(
           createUserQuery,
           [[email, createHash(password), username]],
@@ -44,6 +46,8 @@ class Auth {
         if (!userFound) return res.json({ error: "User not found" });
         if (!isSamePwd(password, userFound.password))
           return res.json({ error: "Wrong password" });
+        delete userFound.password;
+        return res.json(userFound);
       });
     } catch (err) {
       return res.status(400).json({ error: err.message });
