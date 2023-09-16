fdd0f29d4bbedb04d7778a6592d792308a4c5dda
MartinFiori <martinfiori@outlook.com>
2023-09-16 02:27:10 -0300

    getFavoritePosts brings posts from the user and their info

--- back/controllers/Users.controller.js
+++ back/controllers/Users.controller.js
@@ -11,11 +11,11 @@ class Users {
     try {
       const postsQuery = `SELECT * FROM ${posts_table} ORDER BY created_at DESC`;
       db.query(postsQuery, (err, posts) => {
-        if (err) return res.json({ error: err.message })
-        res.json(posts)
-      })
+        if (err) return res.json({ error: err.message });
+        res.json(posts);
+      });
     } catch (err) {
-      return res.json({ error: err.message })
+      return res.json({ error: err.message });
     }
   }
 
@@ -28,7 +28,7 @@ class Users {
         res.json(user_posts);
       });
     } catch (err) {
-      return res.json({ error: err.message })
+      return res.json({ error: err.message });
     }
   }
 
@@ -45,7 +45,7 @@ class Users {
         return res.json("Post created");
       });
     } catch (err) {
-      return res.json({ error: err.message })
+      return res.json({ error: err.message });
     }
   }
 
@@ -56,13 +56,13 @@ class Users {
       if (!id) return res.status(400).json({ error: "Missing user id" });
       if (!content)
         return res.status(400).json({ error: "Cannot edit without content" });
-      const query = `UPDATE ${posts_table} SET content = ? WHERE id = ?; `
+      const query = `UPDATE ${posts_table} SET content = ? WHERE id = ?; `;
       db.query(query, [content, id], (err) => {
         if (err) return res.json({ error: err.message });
-        res.json('Post edited!')
+        res.json("Post edited!");
       });
     } catch (err) {
-      return res.json({ error: err.message })
+      return res.json({ error: err.message });
     }
   }
 
@@ -74,6 +74,7 @@ class Users {
     const findUserQuery = `SELECT * FROM ${users_table} WHERE id = ?;`;
     const findPostQuery = `SELECT * FROM ${posts_table} WHERE id = ?;`;
     const likeQuery = `INSERT INTO ${liked_table} (user_id, post_id) VALUES (?, ?);`;
+
     db.query(findUserQuery, [user_id], (err, [user]) => {
       if (err) {
         return res.status(500).json({ error: err.message });
@@ -92,35 +93,64 @@ class Users {
           if (err) {
             return res.status(500).json({ error: err.message });
           }
-          return res.json('Post liked!');
+          return res.json("Post liked!");
         });
       });
     });
   }
 
-
   static favoritePost(req, res) {
     try {
       const { post_id, user_id } = req.body;
-      if (![post_id, user_id].every(Boolean)) return res.status(400).json({ error: "Missing id's" });
+      if (![post_id, user_id].every(Boolean))
+        return res.status(400).json({ error: "Missing id's" });
       const findUserQuery = `SELECT * FROM ${users_table} WHERE id = ?;`;
       const findPostQuery = `SELECT * FROM ${posts_table} WHERE id = ?;`;
       const favoriteQuery = `INSERT INTO ${favorites_table} (user_id, post_id) VALUES (?, ?);`;
 
       db.query(findUserQuery, [user_id], (err, [user]) => {
         if (err) return res.status(500).json({ error: err.message });
-        if (!user) return res.status(400).json({ error: "User doesn't exist" })
-      })
+        if (!user) return res.status(400).json({ error: "User doesn't exist" });
+      });
       db.query(findPostQuery, [user_id], (err, [post]) => {
         if (err) return res.status(500).json({ error: err.message });
-        if (!post) return res.status(400).json({ error: "User doesn't exist" })
-      })
+        if (!post) return res.status(400).json({ error: "User doesn't exist" });
+      });
       db.query(favoriteQuery, [user_id, post_id], (err) => {
         if (err) return res.status(500).json({ error: err.message });
-      })
-      res.json('Post added to favorites!')
+      });
+      res.json("Post added to favorites!");
     } catch (err) {
-      return res.json({ error: err.message })
+      return res.json({ error: err.message });
+    }
+  }
+
+  static getFavoritePosts(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id)
+        return res.status(400).json({ error: "Must provide an user id" });
+
+      const findUserQuery = `SELECT * FROM ${users_table} WHERE id = ?`;
+      const userFavoritesQuery = `SELECT p.id, p.content, u.img_profile, u.username, p.created_at, p.updated_at
+                                  FROM users AS u
+                                  INNER JOIN favorites AS f
+                                  ON u.id = f.user_id
+                                  INNER JOIN posts AS p
+                                  ON p.id = f.post_id
+                                  WHERE u.id = ?;`;
+
+      db.query(findUserQuery, [id], (err, [user]) => {
+        if (err) return res.status(500).json({ error: err.message });
+        if (!user) return res.status(404).json({ error: "User not found" });
+      });
+
+      db.query(userFavoritesQuery, [id], (err, posts) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.json(posts);
+      });
+    } catch (err) {
+      return res.status(400).json({ error: err });
     }
   }
 
@@ -140,11 +170,12 @@ class Users {
         `INSERT INTO ${users_table} (username,password,email) VALUES (?, ?, ?)`,
         [username, password, email],
         (err) => {
-          if (err) return res.status(400).json({ error: err.message })
+          if (err) return res.status(400).json({ error: err.message });
         }
       );
     }
-    db.query(`INSERT INTO posts (content, user_id)
+    db.query(
+      `INSERT INTO posts (content, user_id)
 VALUES
 ('justo Curae et vel ligula lacinia adipiscing', FLOOR(1 + RAND() * 5)),
 ('vitae justo in Cras libero orci Curae', FLOOR(1 + RAND() * 5)),
@@ -161,12 +192,13 @@ VALUES
 ('ante ipsum Cras quis in', FLOOR(1 + RAND() * 5)),
 ('mattis ac sapien faucibus eu orci sapien Vestibulum Fusce', FLOOR(1 + RAND() * 5)),
 ('Morbi faucibus sapien Fusce vitae auctor adipiscing', FLOOR(1 + RAND() * 5));
-`, (err) => {
-      if (err) return res.status(400).json({ error: err.message })
-    });
+`,
+      (err) => {
+        if (err) return res.status(400).json({ error: err.message });
+      }
+    );
     res.json("todo ok");
   }
-
 }
 
 module.exports = Users;
--- back/routes/users.routes.js
+++ back/routes/users.routes.js
@@ -1,14 +1,22 @@
 const router = require("express").Router();
 const Users = require("../controllers/Users.controller.js");
 
-router.get("/", Users.allPosts);
+router.get("/favorite/:id", Users.getFavoritePosts);
 router.get("/:id", Users.myPosts);
+router.get("/", Users.allPosts);
 
 router.put("/:id", Users.updatePost);
 
-router.post('/like', Users.likePost)
-router.post('/favorite', Users.favoritePost)
+router.post("/like", Users.likePost);
+router.post("/favorite", Users.favoritePost);
 router.post("/insert", Users.insertPosts);
 router.post("/:id", Users.createPost);
 
 module.exports = router;
+// SELECT u.username, p.content
+// FROM users AS u
+// INNER JOIN favorites AS f
+// ON u.id = f.user_id
+// INNER JOIN posts AS p
+// ON p.id = f.post_id
+// WHERE u.id = 3;
